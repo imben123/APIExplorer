be1626c428871c0cc4f64034d53b756def679c7b
Ben Davis <imben123@gmail.com>
2025-07-28 14:32:50 +0100

    Initial Commit

---
+++ APIExplorer/APIExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIExplorerApp.swift
+//  APIExplorer
+//
+//  Created by Ben Davis on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct APIExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIExplorerTests/APIExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIExplorerTests.swift
+//  APIExplorerTests
+//
+//  Created by Ben Davis on 28/07/2025.
+//
+
+import Testing
+@testable import APIExplorer
+
+struct APIExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
